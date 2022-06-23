376499fd529ecaa70ba0c0c428714840540ece1c
Dim0ndr <dimondr@gmail.com>
2022-06-24 01:00:08 +0300

    Initial Commit

---
+++ 2.28_4_ColorPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.28_4_ColorPicker
+//
+//  Created by Dimondr on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
